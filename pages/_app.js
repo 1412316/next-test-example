9462c7c3ea144dc756557b7d63b45994763c4932
Minh <minh.dang@unibiz.io>
2021-05-06 15:53:57 +0700

    change fb code

--- pages/_app.js
+++ pages/_app.js
@@ -3,23 +3,40 @@ import { useEffect } from 'react'
 
 function MyApp({ Component, pageProps }) {
 
-  useEffect(() => {
-    <script async defer crossorigin="anonymous" src="https://connect.facebook.net/vi_VN/sdk.js#xfbml=1&version=v10.0&appId=224405702374158&autoLogAppEvents=1" nonce="bYmFnOn4"></script>
-  }, [])
+  // useEffect(() => {
+  //   <script async defer crossorigin="anonymous" src="https://connect.facebook.net/vi_VN/sdk.js#xfbml=1&version=v10.0&appId=224405702374158&autoLogAppEvents=1" nonce="bYmFnOn4"></script>
+  // }, [])
+
+  // useEffect(() => {
+  //   window.fbAsyncInit = function() {
+  //     FB.init({
+  //       xfbml            : true,
+  //       version          : 'v9.0'
+  //     });
+  //   };
+  
+  //   (function(d, s, id) {
+  //     var js, fjs = d.getElementsByTagName(s)[0];
+  //     if (d.getElementById(id)) return;
+  //     js = d.createElement(s); js.id = id;
+  //     js.src = 'https://connect.facebook.net/vi_VN/sdk/xfbml.customerchat.js';
+  //     fjs.parentNode.insertBefore(js, fjs);
+  //   }(document, 'script', 'facebook-jssdk'));
+  // }, [])
 
   useEffect(() => {
     window.fbAsyncInit = function() {
       FB.init({
         xfbml            : true,
-        version          : 'v9.0'
+        version          : 'v10.0'
       });
     };
-  
+
     (function(d, s, id) {
       var js, fjs = d.getElementsByTagName(s)[0];
       if (d.getElementById(id)) return;
       js = d.createElement(s); js.id = id;
-      js.src = 'https://connect.facebook.net/vi_VN/sdk/xfbml.customerchat.js';
+      js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
       fjs.parentNode.insertBefore(js, fjs);
     }(document, 'script', 'facebook-jssdk'));
   }, [])
--- pages/index.js
+++ pages/index.js
@@ -50,6 +50,10 @@ export default function Home() {
           </a>
           <div class="fb-like" data-href="https://www.facebook.com/H%C3%B4m-nay-l%C3%A0-m%E1%BB%99t-ng%C3%A0y-m%E1%BB%9Bi-100207341700762" data-width="" data-layout="button_count" data-action="like" data-size="small" data-share="true"></div>
           <iframe src="https://www.facebook.com/plugins/like.php?href=https%3A%2F%2Fwww.facebook.com%2FH%25C3%25B4m-nay-l%25C3%25A0-m%25E1%25BB%2599t-ng%25C3%25A0y-m%25E1%25BB%259Bi-100207341700762&width=90&layout=button_count&action=like&size=small&share=false&height=21&appId=224405702374158" width="90" height="21" style={{border: "none", overflow: "hidden"}} scrolling="no" frameBorder="0" allowFullscreen="true" allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"></iframe>
+          
+          <div class="fb-page" data-href="https://www.facebook.com/H&#xf4;m-nay-l&#xe0;-m&#x1ed9;t-ng&#xe0;y-m&#x1edb;i-100207341700762/" data-tabs="timeline" data-width="" data-height="" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true"><blockquote cite="https://www.facebook.com/H&#xf4;m-nay-l&#xe0;-m&#x1ed9;t-ng&#xe0;y-m&#x1edb;i-100207341700762/" class="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/H&#xf4;m-nay-l&#xe0;-m&#x1ed9;t-ng&#xe0;y-m&#x1edb;i-100207341700762/">Hôm nay là một ngày mới</a></blockquote></div>
+          
+          <iframe src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2FH%C3%B4m-nay-l%C3%A0-m%E1%BB%99t-ng%C3%A0y-m%E1%BB%9Bi-100207341700762%2F&tabs=timeline&width=340&height=187&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId=224405702374158" width="340" height="187" style={{border: "none", overflow: "hidden" }} scrolling="no" frameborder="0" allowfullscreen="true" allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"></iframe>
         </div>
       </main>
 
@@ -63,12 +67,16 @@ export default function Home() {
           <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
         </a>
       </footer>
-      <div className="fb-customerchat"
+      {/* <div className="fb-customerchat"
         attribution="setup_tool"
         page_id="100207341700762"
         theme_color="#ff5ca1"
         logged_in_greeting="Chào bạn!"
         logged_out_greeting="Chào bạn!">
+      </div> */}
+      <div class="fb-customerchat"
+        attribution="page_inbox"
+        page_id="109453607947921">
       </div>
     </div>
   )
