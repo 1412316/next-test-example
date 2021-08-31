0df21ea9baec1d0ff817f1f32c203b39da858637
NhatMinh <nhatminhdang1996@gmail.com>
2021-08-31 11:48:11 +0700

    home

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,8 @@ export default function Home() {
 
   return (
     <>
-      <h1>{process.env.NEXT_PUBLIC_PRODUCT_NAME}</h1>
+      <h1>Home page</h1>
+      <div>{process.env.NEXT_PUBLIC_PRODUCT_NAME}</div>
       <div>{process.env.NEXT_PUBLIC_PRODUCT_ID}</div>
     </>
   )
